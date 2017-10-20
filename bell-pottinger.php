4d960960df46e21d496673baa5dbe0c172d2ce2d
desmukh <desmukh@users.noreply.github.com>
2017-10-20 16:18:25 +0500

    Update URLs to permalinks for Bell Pottinger

--- bell-pottinger.php
+++ bell-pottinger.php
@@ -4,10 +4,10 @@
     <title>Bell Pottinger | PR Watch</title>
     <?php include("/home/bahrainwatch/bahrainwatch.org/head.php"); ?>
   </head>
-  
+
   <body>
     <?php include("/home/bahrainwatch/bahrainwatch.org/navbar.php"); ?>
-    
+
     <div class="container">
       <div class="row">
 	<div class="span12">
@@ -15,7 +15,7 @@
             <?php include("subnav.php"); ?>
             <div class="row">
 	      <div class="span9 columns">
-		
+
 		<table class="table table-bordered table-condensed">
 		  <tr>
 		    <td style="width:50%;">
@@ -45,24 +45,24 @@
 		    </td>
 		  </tr>
 		</table>
-		
+
 		<div class="well">
 		  <h3>Relationship with Bahrain Government</h3>
 		  <hr/>
 		  <p>
-		    Bell Pottinger is a major London-based PR agency that has held a number of contracts with the Bahraini government, including <a href="http://www.prweek.com/uk/news/873574/bahrain-passes-brief-bell-pottinger/">one for the Economic Development Board</a> (EDB) since 2009, believed to be worth a seven-figure sum in pounds annually. Bell Pottinger also contracted <a href="qorvis.php">Qorvis Communications</a> to carry out <a href="http://www.documentcloud.org/documents/406325-5483-exhibit-ab-20100806-22.html">public relations work</a> in the United States for Bahrain's Ministry of Foreign Affairs at some point before August 2010.
+		    Bell Pottinger is a major London-based PR agency that has held a number of contracts with the Bahraini government, including <a href="https://web.archive.org/web/20160303232355/https://www.prweek.com/article/873574/bahrain-passes-brief-bell-pottinger">one for the Economic Development Board</a> (EDB) since 2009, believed to be worth a seven-figure sum in pounds annually. Bell Pottinger also contracted <a href="qorvis.php">Qorvis Communications</a> to carry out <a href="http://www.documentcloud.org/documents/406325-5483-exhibit-ab-20100806-22.html">public relations work</a> in the United States for Bahrain's Ministry of Foreign Affairs at some point before August 2010.
 		  </p>
 		  <p>
-		    According to an <a href="http://www.independent.co.uk/news/world/middle-east/bahrain-sentences-four-men-to-death-for-killing-two-policemen-2276390.html">article in the Independent</a> newspaper, Bell Pottinger sends out regular emails to journalist, "including getting out the Manama administration's line at the start of the opposition protests." One email described the Bahraini government's "readiness to receive the injured in hospitals" just before it began <a href="https://en.wikipedia.org/wiki/Bahrain_health_worker_trials">detaining doctors</a>.
+		    According to an <a href="https://web.archive.org/web/20171019195721/http://www.independent.co.uk/news/world/middle-east/bahrain-sentences-four-men-to-death-for-killing-two-policemen-2276390.html">article in the Independent</a> newspaper, Bell Pottinger sends out regular emails to journalist, "including getting out the Manama administration's line at the start of the opposition protests." One email described the Bahraini government's "readiness to receive the injured in hospitals" just before it began <a href="https://en.wikipedia.org/wiki/Bahrain_health_worker_trials">detaining doctors</a>.
 		  </p>
 		  <p>
-		    Bell Pottinger came <a href="http://www.prweek.com/uk/news/1057005/Bell-Pottingers-work-Bahrain-Government-spotlight/">under pressure</a> in February 2011 for holding the contracts with Bahrain even after seven protesters had been killed by security forces in Bahrain, and in April Bell Pottinger <a href="http://www.prweek.com/uk/news/1064256/Bell-Pottingers-Bahrain-brief-suspended-amid-countrys-crisis/">suspended</a> some of its contracts including the EDB one. The chairman of the agency said "<a href="http://www.prweek.com/uk/news/1064256/Bell-Pottingers-Bahrain-brief-suspended-amid-countrys-crisis/">most</a>" -- not all -- of the Bahrain contracts had been suspended, and stressed that it was not necessarily a permanent disengagement.
+		    Bell Pottinger came <a href="https://web.archive.org/web/20170315175114/http://www.prweek.com/article/1057005/bell-pottingers-work-bahrain-government-spotlight">under pressure</a> in February 2011 for holding the contracts with Bahrain even after seven protesters had been killed by security forces in Bahrain, and in April Bell Pottinger <a href="https://web.archive.org/web/20170313034658/http://www.prweek.com/article/1064256/bell-pottingers-bahrain-brief-suspended-amid-countrys-crisis">suspended</a> some of its contracts including the EDB one. The chairman of the agency said "<a href="https://web.archive.org/web/20170313034658/http://www.prweek.com/article/1064256/bell-pottingers-bahrain-brief-suspended-amid-countrys-crisis">most</a>" -- not all -- of the Bahrain contracts had been suspended, and stressed that it was not necessarily a permanent disengagement.
 		  </p>
 		  <p>
 		    Indeed, the disengagement did not last long. In May 2011, Bell Pottinger was awarded a <a href="http://www.documentcloud.org/documents/406923-bahrain-tenders-may2011.html#document/p12/a66758">contract</a> worth US$ 199,000 by Bahrain's Information Affairs Authority for an undefined period of time. In July 2011, the government's holding company Mumtalakat renewed a <a href="http://www.documentcloud.org/documents/406909-bahrain-tenders-jul2011.html#document/p16/a66759">contract</a> with Bell Pottinger, worth US$ 853,000 for a period of one year. In January 2012, Bahrain's Economic Development Board awarded Bell Pottinger with a renewed contract for <a href="http://www.documentcloud.org/documents/406907-bahrain-tenders-jan2012.html#document/p14/a66760">over US$ 10.5 million</a> for a period of two years. And in May 2012 Bell Pottinger's contract with the <a href="http://www.bcdr-aaa.org/">Bahrain Chamber for Dispute Resolution</a> was <a href="http://www.documentcloud.org/documents/408447-bahrain-tender-may2012.html#document/p16/a67354">renewed</a> for a year for about $ 367,000. The information about these contracts have been taken from the website of the government's <a href="http://www.tenderboard.gov.bh/Tendering/AwardedTenders.aspx">Tender Board</a>, and there may be other contracts that are not yet listed.
 		  </p>
 		</div>
-		
+
 		<div class="well">
 		  <h3>Fees and Expenses</h3>
 		  <hr/>
@@ -73,12 +73,12 @@
 		    <li><a href="http://www.documentcloud.org/documents/406917-bahrain-tenders-mar2011.html#document/p11/a66831">March 2011</a>: BD 53,298 (US$ 141,240) [Note: The entry for this says this tender is for work done in 2010 for some reason]</li>
 		    <li><a href="http://www.documentcloud.org/documents/406923-bahrain-tenders-may2011.html#document/p12/a66758">May 2011</a>: BD 75,000 (US$ 198,750)</li>
 		    <li><a href="http://www.documentcloud.org/documents/406909-bahrain-tenders-jul2011.html#document/p16/a66759">July 2011</a>: BD 321,610 (US$ 852,267)</li>
-		    <li><a href="http://www.documentcloud.org/documents/406907-bahrain-tenders-jan2012.html#document/p14/a66760">January 2012</a>: BD 3,973,522 (US$ 10,529,833)</li>	      
+		    <li><a href="http://www.documentcloud.org/documents/406907-bahrain-tenders-jan2012.html#document/p14/a66760">January 2012</a>: BD 3,973,522 (US$ 10,529,833)</li>
 		    <li><a href="http://www.documentcloud.org/documents/408447-bahrain-tender-may2012.html#document/p16/a67354">May 2012</a>: BD 138,700 (US$ 367,555)</li>
 		    <li><b>Total: BD 4,562,130 (US$ 12,098,645)</b></li>
 		  </ul>
 		</div>
-		
+
 		<div class="well">
 		  <h3>Date of Hiring</h3>
 		  <hr/>
@@ -86,15 +86,15 @@
 		    Ongoing since at least 2009.
 		  </p>
 		</div>
-		
+
 		<div class="well">
 		  <h3>Relevant Personnel</h3>
 		  <hr/>
 		  <ul>
-		    <li><a href="http://www.bellpottingerme.com/en/keypeople.php">Mathew Gunther-Bushell</a>, Managing Director of Bell Pottinger's Bahrain office</li>
+		    <li><a href="https://web.archive.org/web/20120223110926/http://www.bellpottingerme.com/en/keypeople.php">Mathew Gunther-Bushell</a>, Managing Director of Bell Pottinger's Bahrain office</li>
 		  </ul>
 		</div>
-		
+
 		<div class="well">
 		  <h3>Key Documents</h3>
 		  <hr/>
@@ -105,23 +105,23 @@
 		    <li>Bahrain Tender Board: <a href="http://www.documentcloud.org/documents/406907-bahrain-tenders-jan2012.html#document/p14/a66760">Awarded tenders of January 2012</a> (see item no. 147)</li>
 		  </ul>
 		</div>
-		
+
 		<div class="well">
 		  <h3>Additional Notes</h3>
 		  <hr/>
 		  <p>
-		    An <a href="http://www.independent.co.uk/news/uk/politics/caught-on-camera-top-lobbyists-boasting-how-they-influence-the-pm-6272760.html">undercover investigation</a> by The Bureau of Investigative Journalism in December 2011 caught senior Bell-Pottinger executives boasting about the "dark arts" they use to bury bad coverage and influence public opinion of countries with bad human rights records.
+		    An <a href="https://web.archive.org/web/20171004163222/http://www.independent.co.uk/news/uk/politics/caught-on-camera-top-lobbyists-boasting-how-they-influence-the-pm-6272760.html">undercover investigation</a> by The Bureau of Investigative Journalism in December 2011 caught senior Bell-Pottinger executives boasting about the "dark arts" they use to bury bad coverage and influence public opinion of countries with bad human rights records.
 		  </p>
 		</div>
-		
+
 		<div class="well">
 		  <h3>Further Reading</h3>
 		  <hr/>
 		  <ul>
-		    <li>PR Week: <a href="http://www.prweek.com/uk/news/873574/bahrain-passes-brief-bell-pottinger/">Bahrain passes brief on to Bell Pottinger</a>, Jan 14, 2009</li>
-		    <li>PR Week: <a href="http://www.prweek.com/uk/news/1057005/Bell-Pottingers-work-Bahrain-Government-spotlight/">Bell Pottinger's work for Bahrain Government under the spotlight</a>, Feb 25, 2009</li>
-		    <li>PR Week: <a href="http://www.prweek.com/uk/news/1064256/Bell-Pottingers-Bahrain-brief-suspended-amid-countrys-crisis/">Bell Pottinger's Bahrain brief suspended amid country's crisis</a>, April 7, 2009</li>
-		    <li>The Independent: <a href="http://www.independent.co.uk/news/world/middle-east/bahrain-sentences-four-men-to-death-for-killing-two-policemen-2276390.html">Bahrain sentences four men to death for killing two policemen</a>, April 29, 2011</li>
+		    <li>PR Week: <a href="https://web.archive.org/web/20160303232355/https://www.prweek.com/article/873574/bahrain-passes-brief-bell-pottinger">Bahrain passes brief on to Bell Pottinger</a>, Jan 14, 2009</li>
+		    <li>PR Week: <a href="https://web.archive.org/web/20170315175114/http://www.prweek.com/article/1057005/bell-pottingers-work-bahrain-government-spotlight">Bell Pottinger's work for Bahrain Government under the spotlight</a>, Feb 25, 2009</li>
+		    <li>PR Week: <a href="https://web.archive.org/web/20170313034658/http://www.prweek.com/article/1064256/bell-pottingers-bahrain-brief-suspended-amid-countrys-crisis">Bell Pottinger's Bahrain brief suspended amid country's crisis</a>, April 7, 2009</li>
+		    <li>The Independent: <a href="https://web.archive.org/web/20171019195721/http://www.independent.co.uk/news/world/middle-east/bahrain-sentences-four-men-to-death-for-killing-two-policemen-2276390.html">Bahrain sentences four men to death for killing two policemen</a>, April 29, 2011</li>
 		  </ul>
 		</div>
 	      </div>

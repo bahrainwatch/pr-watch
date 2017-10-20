4692152da92e651f08db894853387105904c706c
desmukh <desmukh@users.noreply.github.com>
2017-10-20 11:46:18 +0500

    Updates URLs to permalinks for BPC

--- bahrain-press-club.php
+++ bahrain-press-club.php
@@ -4,10 +4,10 @@
     <title>Bahrain Press Club | PR Watch</title>
     <?php include("/home/bahrainwatch/bahrainwatch.org/head.php"); ?>
   </head>
-  
+
   <body>
     <?php include("/home/bahrainwatch/bahrainwatch.org/navbar.php"); ?>
-    
+
     <div class="container">
       <div class="row">
 	<div class="span12">
@@ -15,7 +15,7 @@
             <?php include("subnav.php"); ?>
             <div class="row">
 	      <div class="span9 columns">
-		
+
 		<table class="table table-bordered table-condensed">
 		  <tr>
 		    <td style="width:50%;">
@@ -31,7 +31,7 @@
 		  </tr>
 		  <tr>
 		    <td>
-		      <b>Website:</b> <a href="http://bahrainpressclub.org/">http://bahrainpressclub.org/</a>
+		      <b>Website:</b> <a href="https://web.archive.org/web/20121020033851/http://bahrainpressclub.org/">http://bahrainpressclub.org/</a>
 		    </td>
 		  </tr>
 		  <tr>
@@ -45,22 +45,22 @@
 		    </td>
 		  </tr>
 		</table>
-		
+
 		<div class="well">
 		  <h3>Relationship with Bahrain Government</h3>
 		  <hr/>
 		  <p>
-		    The Bahrain Press Club (BPC) was <a href="http://www.iaa.bh/news-details.aspx?id=178">set up</a> in May 2012 under an edict by the Bahrain government's <a href="https://en.wikipedia.org/wiki/Information_Affairs_Authority">Information Affairs Authority</a>. It describes itself as "a quasi-government organization for media and communication professionals in the Kingdom of Bahrain." However it is unclear how the BPC is a "quasi-government" organization when it appears to be entirely set up and run by the government.
+		    The Bahrain Press Club (BPC) was <a href="https://web.archive.org/web/20171020060501/http://bna.bh/portal/en/news/506734">set up</a> in May 2012 under an edict by the Bahrain government's <a href="https://en.wikipedia.org/wiki/Information_Affairs_Authority">Information Affairs Authority</a>. It describes itself as "a quasi-government organization for media and communication professionals in the Kingdom of Bahrain." However it is unclear how the BPC is a "quasi-government" organization when it appears to be entirely set up and run by the government.
 		  </p>
 		  <p>
-		    The BPC seeks to be a "private members-only club set up for journalists and communication professionals" giving them exclusive <a href="http://bahrainpressclub.org/members/">access</a> to Bahrain government officials and their version of the latest news, and easier access to Bahrain in general. From the looks of it, this appears to be an attempt by the Bahraini government to get closer access to international journalists covering Bahrain by encouraging them to register with the BPC with the carrot of exclusive access.
+		    The BPC seeks to be a "private members-only club set up for journalists and communication professionals" giving them exclusive <a href="https://web.archive.org/web/20121020033855/http://bahrainpressclub.org/members/">access</a> to Bahrain government officials and their version of the latest news, and easier access to Bahrain in general. From the looks of it, this appears to be an attempt by the Bahraini government to get closer access to international journalists covering Bahrain by encouraging them to register with the BPC with the carrot of exclusive access.
 		  </p>
 		  <p>
-		    An overseas liaison office of the BPC in Washington DC was <a href="http://www.iaa.bh/pressReleasedetails.aspx?id=223">set up</a> in June 2012 with much fanfare at the National Press Club. The launch of the liaison office was <a href="http://www.focuswashington.com/2012/06/15/bahrain-press-club-launches-first-overseas-liaison-office-in-washington/">covered by FocusWashington</a>, a website set up by PR firm <a href="qorvis.php">Qorvis Communications</a> which has been working for the Bahrain government since early 2011. The coverage includes an interview of the president of the Information Affairs Authority Shaikh Fawaz bin Mohammed al Khalifa by <a href="http://www.qorvis.com/people/chuck-conconi">Chuck Conconi</a>, vice chairman at Qorvis. A <a href="https://www.youtube.com/watch?v=CmaoU7_0rAo">video report</a> about the launch was also posted on the Youtube channel of the Bahrain Press Club, which may also have been voiced by Conconi.
+		    An overseas liaison office of the BPC in Washington DC was <a href="https://web.archive.org/web/20171020061516/http://bna.bh/portal/en/news/512806">set up</a> in June 2012 with much fanfare at the National Press Club. The launch of the liaison office was <a href="https://web.archive.org/web/20171020063308/https://focuswashington.com/2012/06/15/bahrain-press-club-launches-first-overseas-liaison-office-in-washington/">covered by FocusWashington</a>, a website set up by PR firm <a href="qorvis.php">Qorvis Communications</a> which has been working for the Bahrain government since early 2011. The coverage includes an interview of the president of the Information Affairs Authority Shaikh Fawaz bin Mohammed al Khalifa by <a href="https://web.archive.org/web/20121031045427/http://www.qorvis.com/people/chuck-conconi">Chuck Conconi</a>, vice chairman at Qorvis. A <a href="https://www.youtube.com/watch?v=CmaoU7_0rAo">video report</a> about the launch was also posted on the Youtube channel of the Bahrain Press Club, which may also have been voiced by Conconi.
 		  </p>
 		</div>
-		
-		
+
+
 		<div class="well">
 		  <h3>Fees and Expenses</h3>
 		  <hr/>
@@ -68,7 +68,7 @@
 		    Unknown
 		  </p>
 		</div>
-		
+
 		<div class="well">
 		  <h3>Date of Hiring</h3>
 		  <hr/>
@@ -76,7 +76,7 @@
 		    The Bahrain Press Club was official launched May 3, 2012.
 		  </p>
 		</div>
-		
+
 		<div class="well">
 		  <h3>Relevant Personnel</h3>
 		  <hr/>
@@ -84,17 +84,17 @@
 		    <li>Shaikh Fawaz bin Mohammed Al Khalifa, president of the Information Affairs Authority</li>
 		  </ul>
 		</div>
-		
+
 		<div class="well">
 		  <h3>Further Reading</h3>
 		  <hr/>
 		  <ul>
-		    <li>Bahrain government press release: <a href="http://www.iaa.bh/news-details.aspx?id=178">IAA President Inaugurates Bahrain Press Club</a>, May 3, 2012</li>
-		    <li>Bahrain government press release: <a href="http://www.iaa.bh/pressReleasedetails.aspx?id=223">Bahrain Press Club Launches First Overseas Liaison Office in Washington</a>, June 13, 2012</li>
+		    <li>Bahrain government press release: <a href="https://web.archive.org/web/20171020060501/http://bna.bh/portal/en/news/506734">IAA President Inaugurates Bahrain Press Club</a>, May 3, 2012</li>
+		    <li>Bahrain government press release: <a href="https://web.archive.org/web/20171020061516/http://bna.bh/portal/en/news/512806">Bahrain Press Club Launches First Overseas Liaison Office in Washington</a>, June 13, 2012</li>
 		  </ul>
 		</div>
 	      </div>
-              <?php include("sidenav.php"); ?>	
+              <?php include("sidenav.php"); ?>
               <?php include ("/home/bahrainwatch/bahrainwatch.org/social.php"); ?>
 	    </div>
 	  </div>

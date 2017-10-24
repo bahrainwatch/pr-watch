9f4ed27287d4db03c2edc58e812dc43698f9b675
desmukh <desmukh@users.noreply.github.com>
2017-10-24 14:41:37 +0500

    Update URLs to permalinks for MC Saatchi

--- mc-saatchi.php
+++ mc-saatchi.php
@@ -4,10 +4,10 @@
     <title>M&amp;C Saatchi | PR Watch</title>
     <?php include("/home/bahrainwatch/bahrainwatch.org/head.php"); ?>
   </head>
-  
+
   <body>
     <?php include("/home/bahrainwatch/bahrainwatch.org/navbar.php"); ?>
-    
+
     <div class="container">
       <div class="row">
 	<div class="span12">
@@ -51,7 +51,7 @@
 		  <h3>Relationship with Bahrain Government</h3>
 		  <hr/>
 		  <p>
-		    M&amp;C Saatchi has worked for the Bahraini government for several years and <a href="http://www.ameinfo.com/151914.html">set up shop</a> in Bahrain in 2008. Most notably it was awarded a <a href="http://www.gulf-daily-news.com/NewsDetails.aspx?storyid=213321">$12 million advertising contract</a> by Bahrain's Economic Development Board in April 2008 that resulted in the "<a href="https://www.youtube.com/watch?v=_hrvLKr8zTU">Business Friendly Bahrain</a>" campaign.
+		    M&amp;C Saatchi has worked for the Bahraini government for several years and <a href="https://web.archive.org/web/20080404153611/http://ameinfo.com/151914.html">set up shop</a> in Bahrain in 2008. Most notably it was awarded a <a href="https://web.archive.org/web/20121105205858/http://www.gulf-daily-news.com/NewsDetails.aspx?storyid=213321">$12 million advertising contract</a> by Bahrain's Economic Development Board in April 2008 that resulted in the "<a href="https://www.youtube.com/watch?v=zuhoOl8xTFM">Business Friendly Bahrain</a>" campaign.
 		  </p>
 		  <p>
 		    Around the same time that the Arab Spring protests began in Bahrain in February 2011, M&amp;C Saatchi was awarded a contract by the Bahrain International Circuit to advertise the 2011 Formula 1 race (though it is likely to have applied for the tendered contract well before Feb 2011). This contract was worth <a href="http://www.documentcloud.org/documents/406991-bahrain-tenders-feb2011.html#document/p14/a66861">US$ 130,000</a>, according to official tender documents, though it is not clear what eventually happened as the race ended up being cancelled due to the unrest.
@@ -60,7 +60,7 @@
 		    In July 2011, M&amp;C Saatchi was awarded a five-year contract worth <a href="http://www.documentcloud.org/documents/406909-bahrain-tenders-jul2011.html#document/p14/a66863">BD 5.5 million</a> (US$ 14,575,000) by the Economic Development Board to "develop and implement a comprehensive media and promotional plan" for Bahrain.
 		  </p>
 		  <p>
-		    Then again in November 2011, the Ministry of Culture hired M&amp;C Saatchi to plan and implement a tourism campaign for Bahrain in a contract worth <a href="http://www.documentcloud.org/documents/406992-bahrain-tenders-nov2011.html#document/p8/a66862">BD 196,747</a> (US$ 521,380). It is probably under this contract that these <a href="http://www.2and3.co.uk/uncategorized/bahrain-tourism-mc-saatchi/">two TV commercials</a> were commissioned, to be played on CNN and at the World Economic Forum in Davos.
+		    Then again in November 2011, the Ministry of Culture hired M&amp;C Saatchi to plan and implement a tourism campaign for Bahrain in a contract worth <a href="http://www.documentcloud.org/documents/406992-bahrain-tenders-nov2011.html#document/p8/a66862">BD 196,747</a> (US$ 521,380). It is probably under this contract that these <a href="https://web.archive.org/web/20171024093505/http://www.2and3.co.uk/uncategorized/bahrain-tourism-mc-saatchi/">two TV commercials</a> were commissioned, to be played on CNN and at the World Economic Forum in Davos.
 		  </p>
 		</div>
 
@@ -102,7 +102,7 @@
 		  <h3>Further Reading</h3>
 		  <hr/>
 		  <ul>
-		    <li>Gulf Daily News: <a href="http://www.gulf-daily-news.com/NewsDetails.aspx?storyid=213321">Adverts push in top gear</a>, April 1, 2008</li>
+		    <li>Gulf Daily News: <a href="https://web.archive.org/web/20121105205858/http://www.gulf-daily-news.com/NewsDetails.aspx?storyid=213321">Adverts push in top gear</a>, April 1, 2008</li>
 		  </ul>
 		</div>
 	      </div>

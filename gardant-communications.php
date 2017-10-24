6faa6622eb1c0f5d0160ae3edfc761f7dcefeb7a
desmukh <desmukh@users.noreply.github.com>
2017-10-24 11:42:25 +0500

    Update URLs to permalinks for Gardant

--- gardant-communications.php
+++ gardant-communications.php
@@ -4,10 +4,10 @@
     <title>Gardant Communications | PR Watch</title>
     <?php include("/home/bahrainwatch/bahrainwatch.org/head.php"); ?>
   </head>
-  
+
   <body>
     <?php include("/home/bahrainwatch/bahrainwatch.org/navbar.php"); ?>
-    
+
     <div class="container">
       <div class="row">
 	<div class="span12">
@@ -33,7 +33,7 @@
 		  </tr>
 		  <tr>
 		    <td>
-		      <b>Website:</b> <a href="http://www.gardantcommunications.com/">http://www.gardantcommunications.com/</a>
+		      <b>Website:</b> <a href="https://web.archive.org/web/20111227123456/http://www.gardantcommunications.com/">http://www.gardantcommunications.com/</a>
 		    </td>
 		  </tr>
 		  <tr>
@@ -52,19 +52,19 @@
 		  <h3>Relationship with Bahrain Government</h3>
 		  <hr/>
 		  <p>
-		    Gardant Communications has been working for the Bahrain government since at least 2006 (when it was named Policy Partnership Limited) according to the <a href="http://www.documentcloud.org/documents/406964-gardant-appc-register-31-august-2006.html#document/p31/a66836">APPC register</a>. <a href="http://www.appc.org.uk/en/register/current-register.cfm/gardant-communications">Currently</a>, the account is with the Embassy of Bahrain in London, but at previous times it has simultaneously held a separate account with the Government of Bahrain.
+		    Gardant Communications has been working for the Bahrain government since at least 2006 (when it was named Policy Partnership Limited) according to the <a href="http://www.documentcloud.org/documents/406964-gardant-appc-register-31-august-2006.html#document/p31/a66836">APPC register</a>. <a href="https://web.archive.org/web/20121125085027/http://www.appc.org.uk/en/register/current-register.cfm/gardant-communications">Currently</a>, the account is with the Embassy of Bahrain in London, but at previous times it has simultaneously held a separate account with the Government of Bahrain.
 		  </p>
 		  <p>
-		    Gardant also acted as the <a href="http://www.guardian.co.uk/politics/2011/feb/24/bahrain-government-mps-trip">secretariat</a> for the <a href="http://www.publications.parliament.uk/pa/cm/cmallparty/register/bahrain.htm">UK-Bahrain All-Party Parliamentary Group</a> until at least October 2011, but it appears to have stepped back from this role now. In February 2012, the All-Party group <a href="http://www.mirror.co.uk/sport/formula-1/uk-bahrain-all-party-parliamentary-group-have-backed-684458 ">wrote a letter</a> to the Times of London backing the Formula 1 Grand Prix race in Bahrain, in response to a letter by group of members of the House of Lords who called for it to be cancelled.
+		    Gardant also acted as the <a href="https://web.archive.org/web/20160416233422/http://www.theguardian.com/politics/2011/feb/24/bahrain-government-mps-trip">secretariat</a> for the <a href="https://web.archive.org/web/20101124173635/http://www.publications.parliament.uk/pa/cm/cmallparty/register/bahrain.htm">UK-Bahrain All-Party Parliamentary Group</a> until at least October 2011, but it appears to have stepped back from this role now. In February 2012, the All-Party group <a href="https://web.archive.org/web/20130722121954/http://www.mirror.co.uk/sport/formula-1/uk-bahrain-all-party-parliamentary-group-have-backed-684458">wrote a letter</a> to the Times of London backing the Formula 1 Grand Prix race in Bahrain, in response to a letter by group of members of the House of Lords who called for it to be cancelled.
 		  </p>
 		  <p>
-		    Gardant's founding partner, and the key person for the Bahrain account is Lord <a href="http://www.powerbase.info/index.php/Paddy_Gillford">Paddy Gillford, Earl of Clanwilliam</a>, a former advisor to the Conservative Party. In February 2011, after the start of the Arab Spring protests in Bahrain, Gillford was interviewed on Al Jazeera TV in which he asserted that Bahrain is a "beacon of democracy" and that the anti-government protests involved "a hundred or maybe a thousand people there. this is the vociferous minority, the malcontents. who want to change the nation from the tolerant society at the moment," (as quoted in <a href="http://neoclassics.blogspot.com/2011/03/how-fo-funded-ghaddafi.html">Private Eye issue 1284</a>).
+		    Gardant's founding partner, and the key person for the Bahrain account is Lord <a href="http://www.powerbase.info/index.php/Paddy_Gillford">Paddy Gillford, Earl of Clanwilliam</a>, a former advisor to the Conservative Party. In February 2011, after the start of the Arab Spring protests in Bahrain, Gillford was interviewed on Al Jazeera TV in which he asserted that Bahrain is a "beacon of democracy" and that the anti-government protests involved "a hundred or maybe a thousand people there. this is the vociferous minority, the malcontents. who want to change the nation from the tolerant society at the moment," (as quoted in <a href="https://web.archive.org/web/20120301233347/http://neoclassics.blogspot.com/2011/03/how-fo-funded-ghaddafi.html">Private Eye issue 1284</a>).
 		  </p>
 		  <p>
-		    In June, Gillford got into a <a href="https://alexofarabia.wordpress.com/2011/07/01/washing-your-dirty-laundry-in-public-pr-style/">public spat</a> with Anwar Abdulrahman, the editor of the <a href="http://www.gulf-daily-news.com/">Gulf Daily News</a>, a pro-regime Bahraini newspaper, who is regarded as very close to Bahrain's prime minister. In a front page opinion piece on June 10, 2011, titled "<a href="http://www.gulf-daily-news.com/NewsDetails.aspx?storyid=307539">Overtaken by lies..?</a>" Abdulrahman lashed out at Western PR firms for failing to do enough to protect the Bahrain regime's international reputation, specifically naming Paddy Gillford. 
+		    In June, Gillford got into a <a href="https://web.archive.org/web/20171019225108/https://alexofarabia.com/2011/07/01/washing-your-dirty-laundry-in-public-pr-style/">public spat</a> with Anwar Abdulrahman, the editor of the <a href="https://en.wikipedia.org/wiki/Gulf_Daily_News">Gulf Daily News</a>, a pro-regime Bahraini newspaper, who is regarded as very close to Bahrain's prime minister. In a front page opinion piece on June 10, 2011, titled "<a href="https://web.archive.org/web/20121105210044/http://www.gulf-daily-news.com/NewsDetails.aspx?storyid=307539">Overtaken by lies..?</a>" Abdulrahman lashed out at Western PR firms for failing to do enough to protect the Bahrain regime's international reputation, specifically naming Paddy Gillford.
 		  </p>
 		  <p>
-		    Four days later the Gulf Daily News printed this <a href="http://awurl.com/QKIJQcJPh#first_awesome_highlight">response by Gillford</a> in the letters pages in which he defended himself:
+		    Four days later the Gulf Daily News printed this <a href="https://web.archive.org/web/20171024053120/http://www.gulf-daily-news.com/Print.aspx?storyid=307787">response by Gillford</a> in the letters pages in which he defended himself:
 		  </p>
 		  <blockquote>
 		    "When I defended Bahrain on Al Jazeera news channel recently, calling it a beacon of democracy, I had no idea what events would follow, nor how much I would subsequently be attacked for my loyalty by the British gutter Press. I am proud to defend a country I love, but it would be helpful to have the support of that country's Press instead of unsubstantiated criticism."
@@ -76,7 +76,7 @@
 		    It's hard to imagine that Abdulrahman's public criticism of Gillford could have happened without a nod from higher up in the rungs of power. Nonetheless it appears that Guardant and Gillford were retained by the Bahraini embassy.
 		  </p>
 		  <p>
-		    In December of 2011, when Bahrain's King Hamad flew to London on a visit, Gillford insisted on receiving him at the airport riding a Harley-Davidson with the Bahraini flag painted on it. While following the royal entourage back to the city, there was reportedly <a href="http://www.dailymail.co.uk/news/article-2074873/How-Earl-crashed-Kings-party.html">a collision</a> between Gillford's motorcycle and the limousine carrying Alice Samaan - Bahrain's ambassador to London.
+		    In December of 2011, when Bahrain's King Hamad flew to London on a visit, Gillford insisted on receiving him at the airport riding a Harley-Davidson with the Bahraini flag painted on it. While following the royal entourage back to the city, there was reportedly <a href="http://archive.is/2017.10.24-055448/http://www.dailymail.co.uk/news/article-2074873/How-Earl-crashed-Kings-party.html">a collision</a> between Gillford's motorcycle and the limousine carrying Alice Samaan - Bahrain's ambassador to London.
 		  </p>
 		</div>
 
@@ -117,14 +117,14 @@
 		  <h3>Further Reading</h3>
 		  <hr/>
 		  <ul>
-		    <li>The Bureau of Investigative Journalism: <a href="http://www.thebureauinvestigates.com/2012/04/20/the-inside-track-how-lobbyists-have-helped-launder-bahrains-reputation/">The inside track: how lobbyists have helped launder Bahrain's reputation</a>, April 20, 2012</li>
-		    <li>Private Eye: <a href="http://neoclassics.blogspot.com/2011/03/how-fo-funded-ghaddafi.html">Buffing Up Bahrain</a>, Issue 1284, March 18, 2011</li>
-		    <li>Daily Mail: <a href="http://www.dailymail.co.uk/news/article-2074873/How-Earl-crashed-Kings-party.html">How an Earl crashed the King's party</a>, December 16, 2011</li>
-		    <li>Guardian: <a href="http://www.guardian.co.uk/politics/2011/feb/24/bahrain-government-mps-trip">Bahrain government funded MPs' trip</a>, February 24, 2011</li>
-		    <li>Alex of Arabia: <a href="https://alexofarabia.wordpress.com/2011/07/01/washing-your-dirty-laundry-in-public-pr-style/">Washing your dirty laundry in public PR style</a>, July 1, 2011</li>
-		    <li>Gulf Daily News: <a href="http://www.gulf-daily-news.com/NewsDetails.aspx?storyid=307539">Overtaken by Lies..?</a>, June 10, 2011</li>
-		    <li>Gulf Daily News: <a href="http://awurl.com/QKIJQcJPh#first_awesome_highlight">UK gutter Press versus the truth!</a>, June 14, 2011</li>
-		    <li>Daily Mirror: <a href="http://www.mirror.co.uk/sport/formula-1/uk-bahrain-all-party-parliamentary-group-have-backed-684458">Parliamentary group backs Bahrain race to go ahead</a>, February 14, 2012</li>
+		    <li>The Bureau of Investigative Journalism: <a href="https://web.archive.org/web/20171024063401/https://www.thebureauinvestigates.com/stories/2012-04-20/the-inside-track-how-lobbyists-have-helped-launder-bahrains-reputation">The inside track: how lobbyists have helped launder Bahrain's reputation</a>, April 20, 2012</li>
+		    <li>Private Eye: <a href="https://web.archive.org/web/20120301233347/http://neoclassics.blogspot.com/2011/03/how-fo-funded-ghaddafi.html">Buffing Up Bahrain</a>, Issue 1284, March 18, 2011</li>
+		    <li>Daily Mail: <a href="http://archive.is/2017.10.24-055448/http://www.dailymail.co.uk/news/article-2074873/How-Earl-crashed-Kings-party.html">How an Earl crashed the King's party</a>, December 16, 2011</li>
+		    <li>Guardian: <a href="https://web.archive.org/web/20160416233422/http://www.theguardian.com/politics/2011/feb/24/bahrain-government-mps-trip">Bahrain government funded MPs' trip</a>, February 24, 2011</li>
+		    <li>Alex of Arabia: <a href="https://web.archive.org/web/20171019225108/https://alexofarabia.com/2011/07/01/washing-your-dirty-laundry-in-public-pr-style/">Washing your dirty laundry in public PR style</a>, July 1, 2011</li>
+		    <li>Gulf Daily News: <a href="https://web.archive.org/web/20121105210044/http://www.gulf-daily-news.com/NewsDetails.aspx?storyid=307539">Overtaken by Lies..?</a>, June 10, 2011</li>
+		    <li>Gulf Daily News: <a href="https://web.archive.org/web/20171024053120/http://www.gulf-daily-news.com/Print.aspx?storyid=307787">UK gutter Press versus the truth!</a>, June 14, 2011</li>
+		    <li>Daily Mirror: <a href="https://web.archive.org/web/20130722121954/http://www.mirror.co.uk/sport/formula-1/uk-bahrain-all-party-parliamentary-group-have-backed-684458">Parliamentary group backs Bahrain race to go ahead</a>, February 14, 2012</li>
 		  </ul>
 		</div>
 	      </div>
